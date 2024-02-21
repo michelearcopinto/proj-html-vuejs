aab7107dd0b6975eb4160eebf6a65b570e030359
michelearcopinto <guestphantom1@gmail.com>
2024-02-21 04:25:14 +0100

    feat: first cta main section done

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,18 @@
 <script>
 import AppHeader from "./components/AppHeader.vue";
-import { store } from "./store";
-import axios from "axios";
+import AppMain from "./components/AppMain.vue";
 
 export default {
   components: {
     AppHeader,
+    AppMain,
   },
-  data() {
-    return {
-      store,
-    };
-  },
-  created() {},
-  methods: {},
 };
 </script>
 
 <template>
   <AppHeader />
+  <AppMain />
 </template>
 
 <style lang="scss">
