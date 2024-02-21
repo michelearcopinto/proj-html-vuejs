c5c9ce81ea82cae6a657fc6c468c657fc1de1628
michelearcopinto <guestphantom1@gmail.com>
2024-02-21 02:47:34 +0100

    feat: nav in progress

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,26 @@
 <script>
-import ExampleName from "./components/ExampleName.vue";
+import AppHeader from "./components/AppHeader.vue";
 import { store } from "./store";
 import axios from "axios";
 
 export default {
   components: {
-    ExampleName,
+    AppHeader,
   },
   data() {
     return {
       store,
     };
   },
-  created() {
-    this.requestRandomMail();
-  },
-  methods: {
-    requestRandomMail() {
-      axios
-        .get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then((result) => {
-          store.randomMail = result.data.response;
-        });
-    },
-  },
+  created() {},
+  methods: {},
 };
 </script>
 
 <template>
-  <h1>Hello World</h1>
-  <h2>{{ store.helloValue }}</h2>
-  <h3>axios request: {{ store.randomMail }}</h3>
-  <ExampleName />
+  <AppHeader />
 </template>
 
 <style lang="scss">
 @use "./styles/general.scss";
-
-h2 {
-  font-size: 120px;
-  color: aquamarine;
-}
-
-h3 {
-  font-size: 80px;
-  color: blueviolet;
-}
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,131 @@
+<script>
+// import
+
+export default {
+  name: "AppHeader",
+  data() {
+    return {
+      sectionsListArray: [
+        "courses",
+        "zoom",
+        "pages",
+        "bundles",
+        "course formats",
+        "add course",
+        "demos",
+      ],
+      socialsListArray: [
+        "fab fa-twitter",
+        "fab fa-instagram",
+        "fab fa-behance",
+        "fab fa-dribbble",
+        "fab fa-flickr",
+        "fab fa-git",
+        "fab fa-linkedin",
+        "fab fa-pinterest",
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <header>
+    <nav>
+      <div id="upper-nav">
+        <div id="upper-nav-left-side">
+          <span>english</span>
+          <i class="fas fa-chevron-down"></i>
+        </div>
+        <div id="upper-nav-right-side">
+          <div>
+            <ul>
+              <li v-for="listElement in sectionsListArray">
+                <a href="">{{ listElement }}</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li v-for="social in socialsListArray">
+                <a href=""><i :class="social"></i></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div id="bottom-nav">
+        <figure>
+          <img src="../assets/img/MasterStudy_logo.svg" alt="logo-header" />
+        </figure>
+        <div class="search-container">
+          <span><i class="fas fa-bars"> category</i></span>
+          <div>
+            <input type="text" placeholder="Search courses" />
+            <button><i class="fas fa-magnifying-glass"></i></button>
+          </div>
+        </div>
+        <div>
+          <span><i class="fa-solid fa-bullhorn"></i> Become an Instructor</span>
+          <span><i class="fas fa-briefcase"></i> For Enterprise</span>
+        </div>
+        <div>
+          <span><i class="fa-regular fa-user"></i> Log In</span>
+          <button>SIGN UP</button>
+          <i class="fa-regular fa-bookmark"></i>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+@use "../styles/partials/mixins" as *;
+
+header {
+  padding-top: 20px;
+  nav {
+    #upper-nav {
+      @include header-width;
+      color: $light-color;
+      text-transform: capitalize;
+      display: flex;
+      justify-content: space-between;
+
+      #upper-nav-left-side {
+        display: flex;
+        gap: 5px;
+      }
+
+      #upper-nav-right-side {
+        display: flex;
+        gap: 40px;
+
+        ul {
+          display: flex;
+          gap: 15px;
+
+          a {
+            color: $light-color;
+
+            &:hover {
+              color: black;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  hr {
+    margin-block: 20px;
+  }
+
+  #bottom-nav {
+    @include header-width;
+    display: flex;
+  }
+}
+</style>
